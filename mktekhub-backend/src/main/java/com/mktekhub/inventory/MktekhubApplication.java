4e3c11ff8f50b594818986a589716ada6b474ac7
Muhiddin Kurbonov <muhiddindc@gmail.com>
2025-11-07 21:07:27 -0600

    initial backend setup using start.spring.io, set up postgresql config in application.yml, added springdoc dependency

---
+++ mktekhub-backend/src/main/java/com/mktekhub/inventory/MktekhubApplication.java
@@ -0,0 +1,13 @@
+package com.mktekhub.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MktekhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MktekhubApplication.class, args);
+	}
+
+}
---
+++ mktekhub-backend/src/test/java/com/mktekhub/mktekhub_backend/MktekhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mktekhub.mktekhub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MktekhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
